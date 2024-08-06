6b4de3ed29db07d66d9adb2580081068f1b293bc
newgamecinematic <75565524+newgamecinematic@users.noreply.github.com>
2024-08-06 21:23:14 +0900

    Reef

--- Assets/Scripts/Map/Wind/ShipWindMove.cs
+++ Assets/Scripts/Map/Wind/ShipWindMove.cs
@@ -89,6 +89,15 @@ public class ShipWindMove : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Reef"))
+        {
+            Destroy(gameObject);
+
+        }
+    }
+
     IEnumerator Delay()
     {
         isDelay = true;
