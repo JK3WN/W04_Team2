2c8db9e826a030c3c54eba94bd1081a569b0c21c
JK3WN <yimjay1@gmail.com>
2024-08-06 20:38:23 +0900

    Turn Text Implemented

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -18,13 +18,17 @@ public class TurnManager : MonoBehaviour
     public static TurnList currentTurn = TurnList.P1;
     public event Action<int> WeightStart;
 
+    // YJK, UI 관련 오브젝트들
     public Button endTurnButton;
+    public TMPro.TextMeshProUGUI turnText;
+
     public float eachWeightTime = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         currentTurn = TurnList.P1;
+        ChangeTurnText(currentTurn);
     }
 
     // Update is called once per frame
@@ -44,6 +48,7 @@ public class TurnManager : MonoBehaviour
     public void EndTurnClicked()
     {
         currentTurn = (TurnList)(((int)currentTurn + 1) % 4);
+        ChangeTurnText(currentTurn);
         StartCoroutine("BoatTurn");
     }
 
@@ -56,5 +61,22 @@ public class TurnManager : MonoBehaviour
             yield return new WaitForSeconds(eachWeightTime);
         }
         currentTurn = (TurnList)(((int)currentTurn + 1) % 4);
+        ChangeTurnText(currentTurn);
+    }
+
+    public void ChangeTurnText(TurnList turn)
+    {
+        switch (turn)
+        {
+            case TurnList.P1:
+                turnText.text = "Player 1";
+                break;
+            case TurnList.P2:
+                turnText.text = "Player 2";
+                break;
+            default:
+                turnText.text = "Wait";
+                break;
+        }
     }
 }
