99c9f62ea91f0a55cb63301fe97d94b27e6842a2
taerim <taerimkim2003@gmail.com>
2024-08-08 05:53:21 +0900

    Final Plz

--- Assets/2.Scripts/TurnManager.cs
+++ Assets/2.Scripts/TurnManager.cs
@@ -49,7 +49,7 @@ public class TurnManager : MonoBehaviour
         {
             endTurnButton.gameObject.SetActive(false);
         }
-        apText.text = "AP: " + GameManager.instance.ActionPoints.ToString();
+        apText.text = "Ground: " + GameManager.instance.ActionPoints.ToString();
     }
 
     public void EndTurnClicked()
