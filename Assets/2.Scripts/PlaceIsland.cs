6afc74205ec3351cee1b7aa6eaba6fe5495f5287
JK3WN <yimjay1@gmail.com>
2024-08-07 20:43:42 +0900

    PlaceIsland Implemented
    Currently Doesn't Check If Any Other Objects Are At That Slot

---
+++ Assets/2.Scripts/PlaceIsland.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceIsland : MonoBehaviour
+{
+    public GameObject island, islandPreview;
+
+    public Vector2 intPos;
+    public bool islandReady = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // YJK, 섬 놓을 준비 하는 버튼 누르면 islandReady를 참으로 만들어 다음 내용 진행
+        if (islandReady)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (mousePosition.x > -5 && mousePosition.x < 5 && mousePosition.y > -5 && mousePosition.y < 5)
+            {
+                intPos = new Vector2(Mathf.FloorToInt(mousePosition.x) + 0.5f, Mathf.FloorToInt(mousePosition.y) + 0.5f);
+                islandPreview.SetActive(true);
+                islandPreview.transform.SetPositionAndRotation(intPos, Quaternion.identity);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    islandReady = false;
+                    GameManager.instance.ActionPoints--;
+                    GameObject islandInstance = GameObject.Instantiate(island, intPos, Quaternion.identity);
+                }
+            }
+            else
+            {
+                islandPreview.SetActive(false);
+            }
+        }
+        else
+        {
+            islandPreview.SetActive(false);
+        }
+    }
+}
